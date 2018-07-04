5f3c0a9d10006a8b5a1bc653d1902cc88db2a4c4
Rasmita <rasmita@gmail.com>
2018-07-04 23:42:08 +0530

    a new file added

---
+++ script/myscript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+###################
+#Purpose : To learn shell scripting
+#Owner   : Rasmita Pradhan
+#Version : 1.0
+#Input   : None
+#Output  : None
+###################
+
+
+
+
+#Welcome to bash scripting
+echo "Welcome to bash scripting"
+sleep 5  #wait for 5 sec
+#end of script
+echo "****END****"
